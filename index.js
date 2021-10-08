61b25272a89fb0a511052be59898ae509b81ad5f
Jeff <sparklicorn@users.noreply.github.com>
2021-10-08 15:42:25 -0500

    Use env.PORT with 3000 fallback

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Serve webpacked content in ./dist
 app.use(express.static('dist'));
